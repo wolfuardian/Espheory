90cd1f6129e7fb53055e6c2c3d2c323ceb0ea819
wolfuardian <wolfuardian@gmail.com>
2023-12-20 00:58:11 +0800

    Add Battle Scene prepare to research zenject work flow

---
+++ Espheory/Assets/Scripts/Battle/Main/BattleInstaller.cs
@@ -0,0 +1,19 @@
+#region
+
+using Zenject;
+
+#endregion
+
+namespace Eos.Battle.Main
+{
+    public class BattleInstaller : MonoInstaller
+    {
+        #region Public Methods
+
+        public override void InstallBindings()
+        {
+        }
+
+        #endregion
+    }
+}
