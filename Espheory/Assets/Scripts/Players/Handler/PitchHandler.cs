e7d73f7019d422ca4490bc034855f778f3f0e5e4
eos.chen <eos.chen@nadisystem.com>
2023-12-22 19:07:47 +0800

    Debugging value (WIP)

---
+++ Espheory/Assets/Scripts/Players/Handler/PitchHandler.cs
@@ -0,0 +1,29 @@
+#region
+
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+#endregion
+
+namespace Eos.Players.Handler
+{
+    public class PitchHandler : ITickable
+    {
+        #region Public Variables
+
+        // Your variables here
+
+        #endregion
+
+
+        #region Public Methods
+
+        public void Tick()
+        {
+            // Your code here
+        }
+
+        #endregion
+    }
+}
