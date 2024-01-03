f1dcc8fedc7ab400e4201b98d061a3b58df2dae7
wolfuardian <wolfuardian@gmail.com>
2024-01-04 01:58:36 +0800

    Add Scene Data asset (store Layer Name)

---
+++ Espheory/Assets/Scripts/Gameplay/Scene/Data/SceneData.cs
@@ -0,0 +1,32 @@
+#region
+
+using UnityEngine;
+using Zenject;
+
+#endregion
+
+namespace Eos.Gameplay.Scene.Data
+{
+    [CreateAssetMenu(fileName = "SceneData", menuName = "SceneData", order = 0)]
+    public class SceneData : ScriptableObjectInstaller
+    {
+        #region Private Variables
+
+        [SerializeField] private string groundLayerName = "Ground";
+        [SerializeField] private string wallLayerName   = "Wall";
+        [SerializeField] private string portalLayerName = "Portal";
+
+        #endregion
+
+        #region Public Methods
+
+        public override void InstallBindings()
+        {
+            Container.Bind<string>().WithId("LAYER_GROUND").FromInstance(groundLayerName);
+            Container.Bind<string>().WithId("LAYER_WALL").FromInstance(wallLayerName);
+            Container.Bind<string>().WithId("LAYER_PORTAL").FromInstance(portalLayerName);
+        }
+
+        #endregion
+    }
+}
