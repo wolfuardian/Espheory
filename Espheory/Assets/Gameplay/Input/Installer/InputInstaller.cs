11bb34cf89f926b203c6d86d1daba277baf1038b
wolfuardian <wolfuardian@gmail.com>
2024-09-14 20:39:29 +0800

    ⚡ 重構 Input 模組
    # 理想
    職責分離，依賴關係降低
    然後專注、並限縮，避免隨意拓展功能
    
    # 注意事項
    這個 branch 可能會大幅刪減功能，已重新設計一個更加符合理念的模組塊設計

---
+++ Espheory/Assets/Gameplay/Input/Installer/InputInstaller.cs
@@ -0,0 +1,20 @@
+using Zenject;
+
+namespace Gameplay.Input.Installer
+{
+    public class InputInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind(typeof(IActionTracker)).To<ActionTracker>().AsSingle();
+            Container.Bind(typeof(IActionController), typeof(ITickable)).To<ActionController>().AsSingle();
+            Container.Bind(typeof(IInputState)).To<InputState>().AsSingle();
+            Container.Bind(typeof(IActionService)).To<ActionService>().AsSingle();
+            Container.Bind(typeof(ITickable)).To<PlayerController>().AsSingle();
+            Container.Bind(typeof(IKeyTracker), typeof(ITickable)).To<InputTracker>().AsSingle();
+            Container.Bind(typeof(IKeyboard)).To<Keyboard>().AsSingle();
+            Container.Bind(typeof(IInputHandler),              typeof(ITickable)).To<InputHandler>().AsSingle();
+            Container.Bind(typeof(InputMapper.IPlayerActions), typeof(IInputReader)).To<InputReader>().AsSingle();
+        }
+    }
+}
