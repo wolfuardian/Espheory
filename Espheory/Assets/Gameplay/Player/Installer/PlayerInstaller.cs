f4bc3517699236c053f83fecf51b1c7589fe5934
wolfuardian <wolfuardian@gmail.com>
2024-09-14 20:28:49 +0800

    ⚡ 建立 Player 模組
    會將 PlayerController ,Action 等相關功能整合進去
    
    # 注意事項
    可能得先把這模組處理完才方便去處理其他與＂動作＂有關的模組

---
+++ Espheory/Assets/Gameplay/Player/Installer/PlayerInstaller.cs
@@ -0,0 +1,21 @@
+using Zenject;
+using Gameplay.Input;
+
+namespace Gameplay.Player.Installer
+{
+    public class NavInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind(typeof(IActionTracker)).To<ActionTracker>().AsSingle();
+            Container.Bind(typeof(IActionController), typeof(ITickable)).To<ActionController>().AsSingle();
+            Container.Bind(typeof(IInputState)).To<InputState>().AsSingle();
+            Container.Bind(typeof(IActionService)).To<ActionService>().AsSingle();
+            Container.Bind(typeof(ITickable)).To<PlayerController>().AsSingle();
+            Container.Bind(typeof(IKeyTracker), typeof(ITickable)).To<InputTracker>().AsSingle();
+            Container.Bind(typeof(IKeyboard)).To<Keyboard>().AsSingle();
+            Container.Bind(typeof(IInputHandler),              typeof(ITickable)).To<InputHandler>().AsSingle();
+            Container.Bind(typeof(InputMapper.IPlayerActions), typeof(IInputReader)).To<InputReader>().AsSingle();
+        }
+    }
+}
