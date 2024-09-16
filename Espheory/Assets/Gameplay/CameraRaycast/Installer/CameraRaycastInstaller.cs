1f6da4b63cc8fd051a5223af6f00731f78d1eb82
Eos Wolfuardian <wolfuardian@gmail.com>
2024-09-16 22:43:20 +0800

    feat(camera-raycast): 初始化功能
    - init module

---
+++ Espheory/Assets/Gameplay/CameraRaycast/Installer/CameraRaycastInstaller.cs
@@ -0,0 +1,17 @@
+using Zenject;
+using Gameplay.Input.Core;
+
+namespace Gameplay.CameraRaycast.Installer
+{
+    public class CameraRaycastInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind(typeof(Controls.IPlayerActions)).To<InputReader>().AsSingle().NonLazy(); // NonLazy 一定要加上，否則不會被自動注入
+            Container.Bind(typeof(IInputState)).To<InputState>().AsSingle();
+            Container.Bind(typeof(IInputService)).To<InputService>().AsSingle();
+            Container.Bind(typeof(IInputTrack),          typeof(ITickable)).To<InputTrack>().AsSingle();
+            Container.Bind(typeof(IInputTrackPerformer), typeof(ITickable)).To<InputTrackPerformer>().AsSingle();
+        }
+    }
+}
